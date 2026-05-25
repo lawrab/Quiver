b95ad5ab9fef14fb46e30eef5096eaf50eaebf1a
Lawrence Rabbets <lawrab@users.noreply.github.com>
2026-05-25 20:55:12 +0100

    Remove /quiverdebug diagnostic command

--- Quiver.lua
+++ Quiver.lua
@@ -25,31 +25,3 @@ end
 function Quiver:OnDisable()
     self.UI.Sphere:Hide()
 end
-
-SLASH_QUIVERDEBUG1 = "/quiverdebug"
-SlashCmdList["QUIVERDEBUG"] = function()
-    print("|cff00ff00Quiver Debug|r")
-
-    local numForms = GetNumShapeshiftForms()
-    print("GetShapeshiftForm() = " .. tostring(GetShapeshiftForm()))
-    print("GetNumShapeshiftForms() = " .. tostring(numForms))
-    for i = 1, numForms do
-        local icon, active, castable, spellID = GetShapeshiftFormInfo(i)
-        local name = spellID and GetSpellInfo(spellID) or "?"
-        print(string.format("  Form %d: active=%s spellID=%s name=%s", i, tostring(active), tostring(spellID), tostring(name)))
-    end
-
-    print("Player buffs (UnitBuff):")
-    local i = 1
-    while true do
-        local name, _, icon = UnitBuff("player", i)
-        if not name then break end
-        print(string.format("  Buff %d: %s", i, tostring(name)))
-        i = i + 1
-        if i > 40 then print("  (truncated)") break end
-    end
-
-    local cur = Quiver.Modules.Aspects.current
-    print("Aspects.current = " .. (cur and cur.name or "nil"))
-    print("Sphere.pulsingAspect = " .. tostring(Quiver.UI.Sphere.pulsingAspect))
-end
